ce72cfadf2da32b574fcb07cd962fd2f3a523aaf
Alexandre Pielucha <alexandre.pielucha@gmail.com>
2018-12-31 13:25:13 +0100

    class Transaction (+ UT)

---
+++ src/components/transaction.mjs
@@ -0,0 +1,27 @@
+// @flow
+
+import crypto from 'crypto';
+
+import type Account from './account';
+
+class Transaction {
+  id: string;
+  amount: number;
+  sender: Account;
+  receiver: Account;
+  timestamp: number;
+
+  constructor(amount: number, from: Account, to: Account) {
+    this.id = crypto.randomBytes(32).toString('hex');
+    this.amount = amount;
+    this.sender = from;
+    this.receiver = to;
+    this.timestamp = Date.now();
+  }
+
+  async verifySenderBalance(): Promise<boolean> {
+    return this.sender.balance >= this.amount;
+  }
+}
+
+export default Transaction;
---
+++ tests/components/transaction.spec.mjs
@@ -0,0 +1,45 @@
+// @flow
+
+import Account from '../../src/components/account';
+import Transaction from '../../src/components/transaction';
+
+describe('components/transaction.mjs', () => {
+  describe('class Transaction', () => {
+    describe('constructor()', () => {
+      it('should initialize', () => {
+        const {
+          id,
+          amount,
+          sender,
+          receiver,
+          timestamp,
+        }: Transaction = new Transaction(10, new Account(), new Account());
+
+        expect(typeof id).toBe('string');
+        expect(amount).toBe(10);
+        expect(sender).toBeInstanceOf(Account);
+        expect(receiver).toBeInstanceOf(Account);
+        expect(typeof timestamp).toBe('number');
+      });
+    });
+
+    describe('verifySenderBalance()', () => {
+      it('should return false', async () => {
+        const acc1: Account = new Account();
+        const acc2: Account = new Account();
+        const tran: Transaction = new Transaction(10, acc1, acc2);
+
+        expect(await tran.verifySenderBalance()).toBe(false);
+      });
+
+      it('should return true', async () => {
+        const acc1: Account = new Account();
+        acc1.balance = 10;
+        const acc2: Account = new Account();
+        const tran: Transaction = new Transaction(10, acc1, acc2);
+
+        expect(await tran.verifySenderBalance()).toBe(true);
+      });
+    });
+  });
+});
